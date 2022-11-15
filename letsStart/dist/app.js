bb66919fb2e4d2677b788f73ca8336b64729b838
Youngjin Jo <yjinjo@berkeley.edu>
2022-11-15 10:29:13 +0900

    #1. express 미들웨어 이해하기

--- letsStart/dist/app.js
+++ letsStart/dist/app.js
@@ -3,8 +3,28 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var express = require("express");
 var app_model_1 = require("./app.model");
 var app = express();
+app.use(function (req, res, next) {
+    console.log(req.rawHeaders[1]);
+    console.log("this is logging middleware");
+    next();
+});
+app.get("/cats/som", function (req, res, next) {
+    console.log(req.rawHeaders[1]);
+    console.log("this is som middleware");
+    next();
+});
 app.get('/', function (req, res) {
     res.send({ cats: app_model_1.Cat });
+});
+app.get("/cats/blue", function (req, res) {
+    res.send({ blue: app_model_1.Cat[0] });
+});
+app.get("/cats/som", function (req, res) {
+    res.send({ som: app_model_1.Cat[1] });
+});
+app.use(function (req, res, next) {
+    console.log("this is error middleware");
+    res.send({ error: "404 not found error" });
 });
 app.listen(8000, function () {
     console.log('server is on...');
--- letsStart/src/app.ts
+++ letsStart/src/app.ts
@@ -5,10 +5,38 @@ import { Cat, CatType } from './app.model';
 
 const app: express.Express = express();
 
-app.get('/', (req: express.Request, res: express.Response) => {
-  res.send({ cats: Cat });  // key: value
+// 미들웨어는 use 메서드를 사용한다.
+// next는 다음 라우터로 이동할 수 있는 함수이다. 
+app.use((req, res, next) => {
+  console.log(req.rawHeaders[1]);
+  console.log("this is logging middleware");
+  // 다음 라우터로 실행되게 하려면 next 함수를 호출해야한다. 
+  next();
 })
 
+app.get("/cats/som", (req, res, next) => {
+  console.log(req.rawHeaders[1]);
+  console.log("this is som middleware");
+  // 다음 라우터로 실행되게 하려면 next 함수를 호출해야한다. 
+  next();
+})
+
+app.get('/', (req: express.Request, res: express.Response) => {
+  res.send({ cats: Cat });
+})
+
+app.get("/cats/blue", (req, res) => {
+  res.send({ blue: Cat[0] })
+})
+
+app.get("/cats/som", (req, res) => {
+  res.send({ som: Cat[1] })
+})
+
+app.use((req, res, next) => {
+  console.log("this is error middleware");
+  res.send({ error: "404 not found error" });
+})
 
 // 서버를 열어준다.
 app.listen(8000, () => {
